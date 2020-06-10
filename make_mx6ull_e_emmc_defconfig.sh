e93966c18e7e70242f7e501a845c9a1eb9105888
erised <843707567@qq.com>
2020-06-10 23:28:57 +0800

    2nd commit, on branch master； 移植uboot到开发板，配置LCD和网络外设

---
+++ make_mx6ull_e_emmc_defconfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_e_emmc_defconfig
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+
+
